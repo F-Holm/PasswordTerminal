00d6cb63cd01864bfa6371b99ccc1d7250214c0f
Holm <you@example.com>
2024-12-20 16:27:03 -0300

    genera contraseñas

--- src/PasswordTerminal.cpp
+++ src/PasswordTerminal.cpp
@@ -1,21 +1,27 @@
-// PasswordTerminalC++.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+std::string simbolosEspeciales = "";
+
+char caracterRandom() {
+    return (rand() % 94) + 33;//caracter random entre 33 y 126 ((94 - 1) + 33)
+}
+
+std::string generarContrasenia(unsigned int cantCaracteres) {
+    std::string contra = "";
+    for (;cantCaracteres != 0;cantCaracteres--) contra += caracterRandom();
+    return contra;
+}
+
+void setRand() {
+    srand(std::time(0));
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setRand();
+    std::cout << generarContrasenia(10) << std::endl;
     system("pause");
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
